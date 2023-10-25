57425fa2605c2a38e919f98479b6577b1a734e5c
smorodinovass <81092012+smorodinovass@users.noreply.github.com>
2023-10-25 23:28:56 +0300

    Update function.sh

--- function.sh
+++ function.sh
@@ -4,5 +4,7 @@ export OPENFAAS_URL=http://10.20.14.180:8080 #your IP from faasd (from first VM)
 sudo chmod 666 /var/run/docker.sock
 docker login
 faas-cli login -u admin -p Qf38m8h31AaTFbuDmhHqW5aRsICudJQODObabiPx0kAtE2kLs1UBuKpRuPGSi9y -g http://10.20.14.180:8080 #password and gateway from first VM
-faas-cli new --lang python hello-python
-faas-cli up -f ./hello-python.yml
+faas-cli store deploy Hello-python
+git clone https://github.com/smorodinovass/function_open.git
+cd function_open
+faas-cli up -f ./hello-python3.yml
