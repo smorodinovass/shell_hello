f0fb71f6d1e0d00b1fbb55b7bd29e8d4166e110e
smorodinovass <81092012+smorodinovass@users.noreply.github.com>
2023-10-25 23:11:54 +0300

    Update function.sh

--- function.sh
+++ function.sh
@@ -3,6 +3,6 @@ curl -sSL https://cli.openfaas.com | sudo sh #intall faas-cli
 export OPENFAAS_URL=http://10.20.14.180:8080 #your IP from faasd (from first VM)
 sudo chmod 666 /var/run/docker.sock
 docker login
-faas-cli login -u admin -p mqtO34IiJftdm5Wux9mKQgW1bDTuYgekRL7d4zNaHKpaEez2PAXu3MIoNOuRh4W -g http://10.20.14.180:8080 #password and gateway from first VM
+faas-cli login -u admin -p Qf38m8h31AaTFbuDmhHqW5aRsICudJQODObabiPx0kAtE2kLs1UBuKpRuPGSi9y -g http://10.20.14.180:8080 #password and gateway from first VM
 faas-cli new --lang python hello-python
 faas-cli up -f ./hello-python.yml
