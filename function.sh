5a835e46c62faa6549fddbe79ca5b47e93823c9b
smorodinovass <81092012+smorodinovass@users.noreply.github.com>
2023-10-25 22:54:31 +0300

    Update function.sh

--- function.sh
+++ function.sh
@@ -5,6 +5,5 @@ cd Figlet_OpenFaaS
 sudo chmod 666 /var/run/docker.sock
 docker login
 faas-cli login -u admin -p mqtO34IiJftdm5Wux9mKQgW1bDTuYgekRL7d4zNaHKpaEez2PAXu3MIoNOuRh4W -g http://10.20.14.180:8080 #password and gateway from first VM
-faas-cli store deploy Figlet #install from store for your function
 faas-cli new —lang python hello-python
 faas-cli up -f ./hello-python.yml
